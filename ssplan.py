260bac108ba1dc57b631573f21885897f08725b2
rlagustn92 <73482918+rlagustn92@users.noreply.github.com>
2026-03-27 13:46:08 +0900

    Update ssplan.py

--- ssplan.py
+++ ssplan.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 st.title("엑셀 원본 그대로 보기 테스트")
 
 # 구글 드라이브 공유 링크의 파일 ID
-file_id = "1LP4dGklkFdgjGvNf9qqgMgqiEzVpjLfq"
+file_id = "11ikddEKsE7U_A6SbpSsx4KzauAjSxohr"
 
 # 엑셀 원본 모습을 그대로 보여주는 구글 드라이브 미리보기 URL
 preview_url = f"https://drive.google.com/file/d/{file_id}/preview"
